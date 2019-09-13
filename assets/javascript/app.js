9760f9d38e36f8f3a6a86d52edd930d554c91485
Fahad <fahad011686@gmail.com>
2019-09-13 17:22:27 -0500

    Added game over and sound effects.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,8 @@
-var number = 11; //set initial timer
+var number = 21; //set initial timer
 var intervalId;
 
 function runClock() { //clock functions
-    number = 10;
+    number = 20;
     intervalId = setInterval(decrement, 1000);
 }
 function decrement() {
@@ -23,11 +23,11 @@ function stopClock() {
 
 //questions list
 var qs = [
-    { question: "Who is the President of Apple Computers?", answer: ["Steve Jobs", "Tim Apple", "Bill Gates", "Tim Cook"], s: "two" },
-    { question: "The answer is ZERO", answer: ["ZERO", "1", "2", "3"], s: "one" },
-    { question: "The answer is FOUR", answer: ["0", "1", "2", "FOUR"], s: "four" },
-    { question: "", answer: ["0", "1", "2", "3"], s: "" },
-    { question: "", answer: ["0", "1", "2", "3"], s: "" }
+    { question: "Who is the CEO of Apple Computers?", answer: ["Steve Jobs", "Tim Apple", "Bill Gates", "Tim Cook"], s: "two" },
+    { question: "What does the United States plan on doing in space?", answer: ["Explore it", "Colonize it", "Win at it", "Weaponize it"], s: "three" },
+    { question: "Who is the President of the U.S. Virgin Islands?", answer: ["The U.S. President", "Donald J. Trump", "#45", "Someone else"], s: "four" },
+    { question: "What is a sandwich consisting of cooked patties of ground beef, placed inside a sliced bread bun?", answer: ["Slider", "Hamberder", "Italian Beef", "Jibarito"], s: "two" },
+    { question: "It's freezing and snowing in NYC, what do we need?", answer: ["Global warming", "A jacket", "Heated bus stops", "Hot Hands&#0174"], s: "one" }
 ]
 
 
@@ -45,27 +45,45 @@ var runGame = function (i) {
 var i = 0;
 var selection;
 var points = 0;
+var gameOver = 0;
+
 
 var score = function () {
     points += 1;
     $("#yourScore").html(points);
 
 }
-
 var checkWin = function () {
-    // give point, stop Timer, next Q
     if (selection === solution) {
         score();
+        document.getElementById('audio_yes').play();
+
         console.log("Right answer.");
-        runGame(i += 1);
 
     }
     else {
         console.log("Wrong answer.");
-        runGame(i += 1);
+        document.getElementById('audio_no').play();
+
         // move on with game
     }
+
+    gameOver += 1;
+    checkGameOver();
+
 }
+
+var checkGameOver = function () {
+    if (gameOver == 5) {
+        stopClock();
+        alert("You answered " + points + " out of 5 correctly. You are NOT a geniuous, sorry!");
+        console.log("END GAME.");
+    }
+    else {
+        runGame(i += 1);
+    }
+}
+
 
 //button functions
 $("#btn1").click(function () {
@@ -89,12 +107,12 @@ $(".btn").click(function () {
 
 //starts game and hides Start button after click
 $("#btnStart").click(function () {
-runGame(i);
+    runGame(i);
     var x = document.getElementById("btnStart");
     if (x.style.display === "none") {
-      x.style.display = "block";
+        x.style.display = "block";
     } else {
-      x.style.display = "none";
+        x.style.display = "none";
     }
 
 });
